9e8adc157c13eb7018ade3ca290291f6ef7fd825
Mark Linn <marklinn@xwcsolutions.com>
2012-09-12 10:46:51 -0700

    remove override to force item into menu.  Will let that be added into themes manually

--- app/overrides/menu_additions.rb
+++ app/overrides/menu_additions.rb
@@ -3,7 +3,3 @@ Deface::Override.new(:virtual_path => "spree/layouts/admin",
                      :insert_bottom => "[data-hook='admin_tabs']", 
                      :partial => "spree/admin/shared/press_items_tab")
 
-Deface::Override.new(:virtual_path => "spree/layouts/spree_application", 
-                     :name => "press_items_menu", 
-                     :insert_bottom => "#main-nav-bar[data-hook]", 
-                     :partial => "spree/shared/press_items_menu")
